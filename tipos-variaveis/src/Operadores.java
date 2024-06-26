9a5b9416837d6ba156618cd8c3f94770db06da00
edubabeto <eduardo.babeto@gmail.com>
2024-06-26 20:04:47 -0300

    feat: métodos imprimirRelacionais() e imprimirLogicos() adicionados.

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,11 +1,77 @@
+import java.sql.SQLOutput;
+
 public class Operadores {
     public static void main(String[] args) {
         Operadores op = new Operadores();
         //op.imprimirConcatenar();
         //op.imprimirUnarios();
-        op.imprimirRepeticao();
+        //op.imprimirRepeticao();
+        //op.imprimirTernario();
+        //op.imprimirRelacionais();
+        op.imprimirLogicos();
     }
 
+    public void imprimirLogicos() {
+        boolean condicao1=true;
+        boolean condicao2=false;
+
+        if(condicao1 && condicao2) {
+            System.out.println("As duas condições são verdadeiras.");
+        }
+
+        if(condicao1 || condicao2) {
+            System.out.println("Pelo menos uma das condições é verdadeira.");
+        }
+
+        if(condicao1 && (7 > 4)) {
+            System.out.println("As duas condições são verdadeiras.");
+        }
+    }
+    public void imprimirRelacionais () {
+        int numero1 = 1;
+        int numero2 = 2;
+        String nome1 = "EDUARDO";
+        String nome2 = new String("EDUARDO");
+        boolean simNao = numero1 == numero2;
+        System.out.println("Número um: "+numero1);
+        System.out.println("Número dois: "+numero2);
+        System.out.println("Número um é igual ao número dois? "+simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println("Número um é diferente do número dois? "+simNao);
+
+        simNao = numero1 > numero2;
+        System.out.println("Número um é maior que o número dois? "+simNao);
+
+        simNao = numero1 < numero2;
+        System.out.println("Número um é menor que o número dois? "+simNao);
+
+        simNao = nome1 == nome2;
+        System.out.println("Nome1 é igual ao Nome2? "+simNao);
+
+        simNao = nome1.equals(nome2);
+        System.out.println("Nome1 é igual ao Nome2? "+simNao);
+
+
+    }
+    public void imprimirTernario () {
+        int a, b;
+
+        a = 5;
+        b = 6;
+        String resultado = "";
+        if (a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+        System.out.println(resultado);
+        a = 6;
+        resultado = a==b? "verdadeiro":"falso";
+        int resultado2 = a==b? 1:0;
+        System.out.println(resultado);
+        System.out.println(resultado2);
+    }
     public void imprimirRepeticao() {
         int numero = 5;
         //x repeticao
